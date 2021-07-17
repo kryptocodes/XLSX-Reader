eef07822fc9fbfc5e10af1bf0979973ce9e38938
kryptocodes <srivatsantb@gmail.com>
2021-07-17 12:41:02 +0530

    Added view for the xlsx
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import * as XLSX from "xlsx";
 
 const App = () => {
   const [items, setItems] = useState([]);
-
+  
   const PostRequest = (sheet, values) => {
     console.log(sheet, values);
     return fetch(`http://localhost:8000/api/${sheet}`, {
@@ -23,7 +23,7 @@ const App = () => {
   const OnSubmit = async() => {
     if(items[0] !== "" || items[0] !== undefined){
     for(var i=0;i<items.length;i++){
-      await PostRequest(items[i]?.sheetName,items[i].data)
+      await console.log(items[i]?.sheetName,items[i].data)
     }
   }else{
     console.log("error")
@@ -56,9 +56,9 @@ const App = () => {
   };
 
   return (
-    <div className="container-fluid">
-      <section className="heading">
-        <h4>NodeJS task</h4>
+    <div className="container">
+      <section className="heading text-center">
+        <h4 className="text-center text-white p-4 bg-success">XLS Import and viewer</h4>
         <input
           type="file"
           className="input-field"
@@ -67,12 +67,50 @@ const App = () => {
             readExcel(file);
           }}
         />
+           {/* <button className="btn btn-secondary rounded-0" onClick={() => OnSubmit()}>Submit</button> */}
       </section>
-      <button onClick={() => OnSubmit()}>Submit</button>
+   
+
+  
+      {/* a toggle to change the worksheets accordingly*/}
+        
+
+      {items?.slice().map((item, index) => {
+        var {sheetName,data} = item;
+        return (
+          <div key={index} className="row mx-auto justify-content-center text-center">
+            <div className="col-md-12">
+              <h4>Worksheet Name: {sheetName}</h4>
+            </div>
+            <div className="col-md-8 table-responsive">
+              <table className="table table-bordered table-striped">
+                <thead>
+                  <tr className="bg-success text-white">
+                    {Object.keys(data[0]).map((key, index) => {
+                      return <th key={index}>{key}</th>;
+                    }
+                    )}
+                  </tr>
+                </thead>
+                <tbody>
+                  {data.map((item, index) => {
+                    return <tr key={index}>
+                      {Object.keys(item).map((key, index) => {
+                  
+                        return <td key={index}>{item[key]}</td>;
+                      })}
+                    </tr>
+                  }
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        );
+      }
+      )} 
     </div>
   );
-
-  return <div></div>;
 };
 
 export default App;
